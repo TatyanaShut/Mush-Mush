4a7f586d3b8c6a21c5162fcad871e77d4fc65c9d
Dzmitry Shabailau <budulayreturns@gmail.com>
2019-07-14 18:22:47 +0300

    custom pins

---
+++ Mush-Mush/Mush-Mush/MushroomAnnotationView.m
@@ -0,0 +1,26 @@
+//
+//  MushroomAnnotationView.m
+//  Mush-Mush
+//
+//  Created by Дмитрий on 7/14/19.
+//  Copyright © 2019 Tatyana Shut. All rights reserved.
+//
+
+#import "MushroomAnnotationView.h"
+
+@implementation MushroomAnnotationView
+
+- (id<MKAnnotation>)annotation {
+    
+    
+    return nil;
+}
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
